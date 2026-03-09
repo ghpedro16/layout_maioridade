39930ae4823b84c17f9afeb2e5d2fa80ae91b92e
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-03-09 09:36:26 -0300

    layout concluido, validador de idade com componentes de estado no jetpack compose

--- app/src/main/java/com/example/layoutmaioridade/MainActivity.kt
+++ app/src/main/java/com/example/layoutmaioridade/MainActivity.kt
@@ -5,17 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.layoutmaioridade.ui.theme.LayoutMaioridadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,25 +56,121 @@ fun StartScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
             .background(Color.White)
-            .fillMaxSize()
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "Qual é a sua idade?"
+                text = "Qual é a sua idade?",
+                color = Color.Blue,
+                fontSize = 32.sp
             )
         }
+
+        Spacer(modifier = Modifier.height(12.dp))
 
         Row(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "Aperte os botões para informar a sua idade"
+                text = "Aperte os botões para informar a sua idade",
+                color = Color.Black,
+                fontSize = 18.sp
             )
         }
 
+        Spacer(modifier = Modifier.height(24.dp))
+
+        var idade by remember { mutableStateOf(0) }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(
+                text = "$idade",
+                color = Color.Black,
+                fontSize = 40.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row() {
+            Button(
+                onClick = {
+                    if(idade > 0) {
+                        idade -= 1
+                    }
+                },
+                modifier = Modifier
+                    .padding(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue
+                ),
+                shape = RoundedCornerShape(5.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Icon subtrair",
+                    tint = Color.White
+                )
+            }
+
+            Button(
+                onClick = {
+                    if (idade <= 99) {
+                        idade += 1
+                    }
+                },
+                modifier = Modifier
+                    .padding(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue
+                ),
+                shape = RoundedCornerShape(5.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    contentDescription = "Icon somar",
+                    tint = Color.White
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(28.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            if (idade >= 18){
+                Text(
+                    text = "Você é MAIOR de idade",
+                    color = Color.Blue,
+                    fontSize = 32.sp
+                )
+            }else{
+                Text(
+                    text = "Você é MENOR de idade",
+                    color = Color.Blue,
+                    fontSize = 32.sp
+                )
+            }
+
+        }
     }
 }
